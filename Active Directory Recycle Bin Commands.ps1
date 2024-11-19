12113534da68932b2cbdc744b7a2ad90a709a529
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-11-19 14:48:06 +0200

    Create Active Directory Recycle Bin Commands.ps1

---
+++ Active Directory Recycle Bin Commands.ps1
@@ -0,0 +1,38 @@
+# Import Active Directory module
+Import-Module ActiveDirectory
+
+# Check replication status
+repadmin /replsummary
+
+# Enable Recycle Bin
+Enable-ADOptionalFeature -FeatureName 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target 'learnedlessons.com'
+
+# Verify Recycle Bin status
+Get-ADOptionalFeature -Filter 'name -like "recycle*"'
+
+# Create test user
+New-ADUser -Name "TestUser1" `
+           -GivenName "Test" `
+           -Surname "User" `
+           -SamAccountName "TestUser1" `
+           -UserPrincipalName "TestUser1@learnedlessons.com" `
+           -Enabled $true `
+           -AccountPassword (ConvertTo-SecureString "SomePass1" -AsPlainText -Force)
+
+# Verify user creation
+Get-ADUser TestUser1
+
+# Delete test user
+Remove-ADUser TestUser1 -Confirm:$false
+
+# Search for deleted user
+Get-ADObject -Filter {samAccountName -eq "TestUser1"} -IncludeDeletedObjects -Properties *
+
+# Store deleted user info in variable
+$deletedUser = Get-ADObject -Filter {samAccountName -eq "TestUser1"} -IncludeDeletedObjects -Properties *
+
+# Restore deleted user
+Restore-ADObject -Identity $deletedUser
+
+# Verify user restoration
+Get-ADUser TestUser1
